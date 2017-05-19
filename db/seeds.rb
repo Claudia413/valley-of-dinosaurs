7187bdcb229782a10055ed047592762dccd98c27
Claudia413 <cmjengelsman@gmail.com>
2017-05-19 13:11:32 +0200

    Display a list of dinosaurs

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
 dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+dino4 = Dinosaur.create!( { name: "Iguanodandy", age: rand(100), image_url: "https://goo.gl/wB6q9q" } )
+dino5 = Dinosaur.create!( { name: "Mostardsaurus", age: rand(100), image_url: "https://goo.gl/aoZsYt" } )
+dino6 = Dinosaur.create!( { name: "Hungrysaurus", age: rand(100), image_url: "https://goo.gl/EqRlgP" } )
